6928b15dcc1a3e593630369146a2a17c0e481612
johansvedberg <johan@svedberg.biz>
2017-04-25 22:19:45 +0200

    Added some prototype-stuff

---
+++ color-box.js
@@ -0,0 +1,18 @@
+
+  AFRAME.registerComponent('color-box', {
+  init: function () {
+
+    this.el.addEventListener('click', function (evt) {
+    this.setAttribute('material', 'color', getRandomColor());
+    //console.log('I was clicked at: ', evt.detail.intersection.point);
+    });
+  }
+});
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
